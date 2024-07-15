2b1ae8e7237c2fce4f56efa4317162a324a696f5
ansari-sahil <sahilansari20031010@gmail.com>
2024-07-15 07:31:28 +0530

    basic full stack

---
+++ todo-list/server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
+const pageRoutes = require("./routes/pages");
+
+const PORT = 8080;
+// expres instance
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth',authRoutes)
+app.use('/api/todos',todoRoutes)
+
+app.use('/',pageRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
