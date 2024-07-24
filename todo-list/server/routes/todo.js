0431792502accb24f834958d5204908e4f3b0fe5
ansari-sahil <sahilansari20031010@gmail.com>
2024-07-24 16:50:29 +0530

    problem with the ui

--- todo-list/server/routes/todo.js
+++ todo-list/server/routes/todo.js
@@ -4,96 +4,120 @@ const { isAuthorised } = require("../middlewares");
 const router = require("express").Router();
 
 router.get("/", isAuthorised, async (req, res) => {
-    try {
-        const { user } = req;
-        const todos = await Todo.find({userId:user.id})
-        return res.json({
-            data: todos,
-            success: true,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-          message: "Internal server error",
-          success: false,
-        });
+  try {
+    const { user } = req;
+    const todos = await Todo.find({ userId: user.id });
+    return res.json({
+      data: todos,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+});
+
+router.post("/", isAuthorised, async (req, res) => {
+  try {
+    const { user } = req;
+    const { title } = req.body;
+    if (!title) {
+      return res.status(400).json({
+        message: "title is required",
+        success: false,
+      });
     }
-})
+    const todo = await Todo.create({
+      title,
+      complete: false,
+      userId: user.id,
+    });
+    return res.json({
+      data: todo,
+      success: true,
+      message: "todo created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+});
 
-router.post('/',isAuthorised,async(req,res)=>{
-    try{
-        const {user} = req
-        const {title} = req.body
-        if(!title){
-            return res.status(400).json({
-                message:'title is required',
-                success:false
-            })
-        }
-        const todo =await Todo.create( {
-            title,
-            complete:false,
-            userId:user.id
-        });
-        return res.json({
-            data:todo,
-            success:true,
-            message:'todo created successfully'
+router.patch("/:id", isAuthorised, async (req, res) => {
+  try {
+    const { user } = req;
+    const { id: todoId } = req.params;
+    const { title, complete } = req.body;
+    // check if todo exists
+    const todo = Todo.findOne({ _id: todoId });
+    if (todo.userId !== user.id) {
+      return res.status(401).json({
+        message: "unauthorized",
+        success: false,
+      });
+    }
+    // check if todo belongs to the user
+    if (todo.userId !== user.id) {
+      return res.status(401).json({
+        message: "unauthorized",
+        success: false,
+      });
+    }
+    // update the todo
+    if (title !== undefined) {
+      todo.title = title;
+    }
+    if (complete !== undefined) {
+      todo.complete = complete;
+    }
+    await Todo.findOneAndUpdate({ _id: todoId }, todo, { new: true });
+    return res.json({
+      data: todo,
+      success: true,
+      message: "todo updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+});
 
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-          message: "Internal server error",
-          success: false,
-        });
-      }
-})
-
-router.patch('/:id',isAuthorised,async(req,res)=>{
-    try{
-        const {user} = req
-        const {id:todoId} = req.params
-        const {title,complete} = req.body
-        // check if todo exists
-        const todos = await fs.readFile('./db/todos.json','utf8')
-        const parsedTodos = JSON.parse(todos)
-        const todo = parsedTodos.find((todo)=>todo.id === todoId)
-        if(!todo){
-            return res.status(404).json({
-                message:'todo not found',
-                success:false
-            })
-        }
-        // check if todo belongs to the user
-        if(todo.userId !== user.id){
-            return res.status(401).json({
-                message:'unauthorized',
-                success:false
-            })
-        }
-        // update the todo
-        if(title !== undefined){
-            todo.title = title
-        }
-        if(complete !== undefined){
-            todo.complete = complete
-        }
-        await Todo.findOneAndUpdate({_id:todoId},todo,{new:true})
-        return res.json({
-            data:todo,
-            success:true,
-            message:'todo updated successfully'
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-          message: "Internal server error",
-          success: false,
-        });
-      }
-})
-
-router.delete('/:id',isAuthorised,async(req,res)=>{})
-
+router.delete("/:id", isAuthorised, async (req, res) => {
+  try {
+    const { user } = req;
+    const { id: todoId } = req.params;
+    // check if todo exists
+    const todo = Todo.findOne({ _id: todoId });
+    if (!todo) {
+      return res.status(404).json({
+        message: "todo not found",
+        success: false,
+      });
+    }
+    // check if todo belongs to the user
+    if (todo.userId !== user.id) {
+      return res.status(401).json({
+        message: "unauthorized",
+        success: false,
+      });
+    }
+    // delete the todo
+    await Todo.findOneAndDelete({ _id: todoId }, todo, { new: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+});
 module.exports = router;
